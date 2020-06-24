941e83ba16898344e451b2bdfb85e6028172c067
Joey Petrone <joey.petrone@gmail.com>
2020-06-23 19:49:29 -0500

    routing complete

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -6,7 +6,11 @@ class Auth extends React.Component {
   render() {
     return (
       <div className="Auth">
-        <h1>Auth Component</h1>
+        <h1><img className="pf-logo" src="https://i.imgur.com/7UYwPwc.png" alt="blue cap" /> Pin Finder</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer className="text-muted">
+          <small>Copyright 2020 Twitter, Inc and other contributors Graphics licensed under CC-BY 4.0: <a href="https://creativecommons.org/licenses/by/4.0/" alt="link to Twemoji Creative Commons License">https://creativecommons.org/licenses/by/4.0/</a></small>
+        </footer>
       </div>
     );
   }
