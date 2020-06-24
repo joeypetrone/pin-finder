5c953ae1be84ed68f15da8d342beb607ef616cd6
Joey Petrone <joey.petrone@gmail.com>
2020-06-23 21:55:52 -0500

    auth component complete

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home Component</h1>
+        <h3>My Properties</h3>
       </div>
     );
   }
