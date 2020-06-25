c2ef5e16ec9ee2958d3cbab5f2571267adb9900c
Joey Petrone <joey.petrone@gmail.com>
2020-06-25 15:04:37 -0500

    all property information displaying

--- src/components/pages/SingleProperty/SingleProperty.js
+++ src/components/pages/SingleProperty/SingleProperty.js
@@ -35,16 +35,28 @@ class SingleProperty extends React.Component {
       <div className="SingleProperty">
         <h3>{property.name}</h3>
         <h4>{property.address}</h4>
-        <Link className="btn btn-primary" to={addPinLink}>Add Pin</Link>
-        <div className="justify-content-center">
-          <div className="row card mb-3">
-            <img src={property.imageUrl} className="card-img-top p-4 border" alt={property.name} />
-            <p className="card-text m-3">{property.description}</p>
+        <div class="card flex-row flex-wrap justify-content-center">
+          <div class="card-header border-0">
+            <img src={property.imageUrl} className="card-img" alt="" />
+          </div>
+          <div class="card-block px-2">
+            <h5 class="card-title">Owner: {property.owner}</h5>
+            <p class="card-text"><h6>Description:</h6> {property.description}</p>
+          </div>
+          <div class="card-block px-2">
+            <h6 class="card-title">Property Area: {property.squareFeet} SQ.FT.</h6>
+            <p class="card-text">Cordinates: {property.centerLat} {property.centerLng}</p>
             <div className="mb-3">
               <Link className="btn btn-warning m-2" to={editPropertyLink}>Edit</Link>
               <button className="btn btn-danger m-2" onClick={this.removeSingleProperty}>Delete</button>
             </div>
           </div>
+        </div>
+        <div className="justify-content-center">
+          <div className="row card mb-3">
+            <img src="https://ontheworldmap.com/usa/city/nashville/large-detailed-map-of-nashville.jpg" className="card-img-top p-4 border" alt={property.name} />
+          </div>
+          <Link className="btn btn-primary" to={addPinLink}>Add Pin</Link>
         </div>
       </div>
     );
