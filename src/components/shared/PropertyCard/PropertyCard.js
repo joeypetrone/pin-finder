2f1086fa8163947661bd4f61c0f6bee8918bed9f
Joey Petrone <joey.petrone@gmail.com>
2020-06-24 21:40:20 -0500

    cards are displaying

--- src/components/shared/PropertyCard/PropertyCard.js
+++ src/components/shared/PropertyCard/PropertyCard.js
@@ -5,8 +5,15 @@ import './PropertyCard.scss';
 class PropertyCard extends React.Component {
   render() {
     return (
-      <div className="PropertyCard">
-        <h1>PropertyCard Component</h1>
+      <div className="PropertyCard col-sm-12 col-md-3 mb-4">
+        <div class="card">
+          <img class="card-img-top" src="..." alt="property card" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button href="#" class="btn btn-primary">Go somewhere</button>
+          </div>
+        </div>
       </div>
     );
   }
